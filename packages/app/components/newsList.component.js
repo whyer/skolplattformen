9f824ebb3ffd62faaccaef281e4ba30e8ce1c2c2
Christian Landgren <christian@landgren.nu>
2021-02-20 18:24:44 +0100

    feat: Added illustrations in news page

--- packages/app/components/newsList.component.js
+++ packages/app/components/newsList.component.js
@@ -1,18 +1,29 @@
 import React from 'react'
-import { StyleSheet } from 'react-native'
-import { List } from '@ui-kitten/components'
+import {StyleSheet, Image, View } from 'react-native'
+import { List, Text } from '@ui-kitten/components'
 import { NewsListItem } from './newsListItem.component'
 
 export const NewsList = ({ news }) => {
   return (
-    <List
-      style={styles.container}
-      contentContainerStyle={styles.contentContainer}
-      data={news}
-      renderItem={(info) => (
-        <NewsListItem key={info.item.id} item={info.item} />
-      )}
-    />
+    <>
+     <Image
+        source={require('../assets/girls.png')}
+        style={{height: 200, width: '80%', alignContent:'center'}}
+      />
+      <Text
+        category="h5"
+        style={{position: 'absolute', right: 10, top: 60, width: 150}}>
+        Nyheter från Skolan
+      </Text>
+      <List
+        style={styles.container}
+        contentContainerStyle={styles.contentContainer}
+        data={news}
+        renderItem={(info) => (
+          <NewsListItem key={info.item.id} item={info.item} />
+        )}
+      />
+    </>
   )
 }
 
